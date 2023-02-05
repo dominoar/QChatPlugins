d745e0a5d9edaeefc37f8f7476537599557ca573
dominoar <duominuoaier@gamil.com>
2023-02-06 01:23:53 +0800

    v1.0.2 · 新的插件，支持NovelAi故事模式。

---
+++ NovelAi/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@CreateTime     : 2023/1/30 12:17
+@Author         : DominoAR and group(member_name)
+@File           : __init__.py.py
+@LastEditTime   : 
+"""
---
+++ NovelAi/novelai_api/__init__.py
@@ -0,0 +1,2 @@
+from plugins.NovelAi.novelai_api.NovelAI_API import NovelAIAPI
+from plugins.NovelAi.novelai_api.NovelAIError import NovelAIError
