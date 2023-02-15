a9ddcd66221f96c1413cab1e23834995478ae4f2
dominoar <duominuoaier@gamil.com>
2023-02-15 18:46:13 +0800

    优化: 移除不必要的插件

--- NovelAi/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@CreateTime     : 2023/1/30 12:17
-@Author         : DominoAR and group(member_name)
-@File           : __init__.py.py
-@LastEditTime   : 
-"""
--- NovelAi/novelai_api/__init__.py
+++
@@ -1,2 +0,0 @@
-from plugins.NovelAi.novelai_api.NovelAI_API import NovelAIAPI
-from plugins.NovelAi.novelai_api.NovelAIError import NovelAIError
